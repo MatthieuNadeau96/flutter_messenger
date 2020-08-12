d1934e8b3f4a46ec70e7993d2cbbde697bad603f
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-08-12 14:05:36 -0400

    Displaying usernames

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -1,10 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble(this.message, this.isMe, {this.key});
+  MessageBubble(this.message, this.username, this.isMe, {this.key});
 
   final Key key;
   final String message;
+  final String username;
   final bool isMe;
 
   @override
@@ -12,25 +14,41 @@ class MessageBubble extends StatelessWidget {
     return Row(
       mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
-        Container(
-          constraints: BoxConstraints(
-              maxWidth: MediaQuery.of(context).size.width * 0.85),
-          margin: EdgeInsets.symmetric(
-            vertical: 5,
-            horizontal: 5,
-          ),
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: isMe ? Theme.of(context).primaryColor : Colors.grey[300],
-          ),
-          child: Text(
-            message,
-            style: TextStyle(
-              color: isMe ? Colors.white : Colors.black,
-              fontSize: 16,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (!isMe)
+              Container(
+                margin: EdgeInsets.only(left: 20),
+                child: Text(
+                  username,
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            Container(
+              constraints: BoxConstraints(
+                  maxWidth: MediaQuery.of(context).size.width * 0.85),
+              margin: EdgeInsets.symmetric(
+                vertical: 5,
+                horizontal: 5,
+              ),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: isMe ? Theme.of(context).primaryColor : Colors.grey[300],
+              ),
+              child: Text(
+                message,
+                style: TextStyle(
+                  color: isMe ? Colors.white : Colors.black,
+                  fontSize: 16,
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ],
     );
